114df32e96ae529e83b0c0b4196dda88db9b64e1
Marti <mcostama25@alumnes.ub.edu>
2023-11-29 20:15:51 +0100

    Lab 22 first part done

--- src/test/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderControllerTest.java
+++ src/test/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderControllerTest.java
@@ -1,101 +1,102 @@
-// package es.upm.dit.apsv.transportationorderserver;
+package es.upm.dit.apsv.transportationorderserver;
 
-// import static org.mockito.Mockito.*;
-// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-// import java.io.BufferedReader;
-// import java.io.FileReader;
-// import java.io.IOException;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-// import org.junit.jupiter.api.Test;
-// import org.mockito.InjectMocks;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-// import org.springframework.boot.test.mock.mockito.MockBean;
-// import org.springframework.core.io.ClassPathResource;
-// import org.springframework.http.MediaType;
-// import org.springframework.test.web.servlet.MockMvc;
-// import org.springframework.test.web.servlet.MvcResult;
-// import org.springframework.test.web.servlet.RequestBuilder;
-// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-// import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-// import es.upm.dit.apsv.transportationorderserver.controller.TransportationOrderController;
-// import es.upm.dit.apsv.transportationorderserver.model.TransportationOrder;
-// import es.upm.dit.apsv.transportationorderserver.repository.TransportationOrderRepository;
+import es.upm.dit.apsv.transportationorderserver.controller.TransportationOrderController;
+import es.upm.dit.apsv.transportationorderserver.model.TransportationOrder;
+import es.upm.dit.apsv.transportationorderserver.repository.TransportationOrderRepository;
 
-// @WebMvcTest(TransportationOrderController.class)
-// public class TransportationOrderControllerTest {
+@WebMvcTest(TransportationOrderController.class)
+public class TransportationOrderControllerTest {
 
-//     @InjectMocks
-//     private TransportationOrderController business;
+    @InjectMocks
+    private TransportationOrderController business;
 
-//     @MockBean
-//     private TransportationOrderRepository repository;
+    @MockBean
+    private TransportationOrderRepository repository;
 
-//     @Autowired
-//     private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-//     // ====TEST 1====
-//     @Test
-//     public void testGetOrders() throws Exception {
-//     //call GET "/transportationorders" application/json
+    // ====TEST 1==== returns OK if the query GET "/transportationorders" for all the transportation orders returns a list of 20 items
+    @Test
+    public void testGetOrders() throws Exception {
+    //call GET "/transportationorders" application/json
 
-//     when(repository.findAll()).thenReturn(getAllTestOrders());
+    when(repository.findAll()).thenReturn(getAllTestOrders());
 
-//     RequestBuilder request = MockMvcRequestBuilders
-//     .get("/transportationorders")
-//     .accept(MediaType.APPLICATION_JSON);
+    RequestBuilder request = MockMvcRequestBuilders
+    .get("/transportationorders")
+    .accept(MediaType.APPLICATION_JSON);
 
-//     MvcResult result = mockMvc.perform(request)
-//     .andExpect(status().isOk())
-//     .andExpect(jsonPath("$", hasSize(20)))
-//     .andReturn();
-//     }
+    MvcResult result = mockMvc.perform(request)
+    .andExpect(status().isOk())
+    .andExpect(jsonPath("$", hasSize(20)))
+    .andReturn();
+    }
 
-//     // ====TEST 2======
-// @Test
+    // ====TEST 2====== returns OK if it finds a given transportation order using the GET "/transportationorders/{truck}
+@Test
+public void testGetOrder() throws Exception {
 
-// public void testGetOrder() throws Exception {
+    //call GET "/transportationorders/{truck}"  application/json
+    when(repository.findById("8962ZKR")).thenReturn(Optional.of(
+            new TransportationOrder("28","8962ZKR",1591682400000L,
+            40.4562191,-3.8707211,1591692196000L,42.0206372,-4.5330132,
+            0,0.0,0.0,0)));
 
-//     //call GET "/transportationorders/{truck}"  application/json
-//     when(repository.findById("8962ZKR")).thenReturn(Optional.of(
-//             new TransportationOrder("28","8962ZKR",1591682400000L,
-//             40.4562191,-3.8707211,1591692196000L,42.0206372,-4.5330132,
-//             0,0.0,0.0,0)));
+    // now write the rest of the test case...
+    RequestBuilder request = MockMvcRequestBuilders
+    .get("/transportationorders")
+    .accept(MediaType.APPLICATION_JSON);
 
-//     // now write the rest of the test case...
-//     RequestBuilder request = MockMvcRequestBuilders
-//     .get("/transportationorders")
-//     .accept(MediaType.APPLICATION_JSON);
-
-//     MvcResult result = mockMvc.perform(request)
-//     .andExpect(status().isOk())
-//     // .andExpect(jsonPath("$", hasSize(20)))
-//     .andReturn();
-//     }
+    MvcResult result = mockMvc.perform(request)
+    .andExpect(status().isOk())
+    // .andExpect(jsonPath("$", hasSize(20)))
+    .andReturn();
+    }
 
 
-//     private List<TransportationOrder> getAllTestOrders() {
+    private List<TransportationOrder> getAllTestOrders() {
 
-//         ObjectMapper objectMapper = new ObjectMapper();
-//         ArrayList<TransportationOrder> orders = new ArrayList<TransportationOrder>();
-//         TransportationOrder order = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayList<TransportationOrder> orders = new ArrayList<TransportationOrder>();
+        TransportationOrder order = null;
 
-//         try (BufferedReader br = new BufferedReader(new FileReader(new ClassPathResource("orders.json").getFile()))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(new ClassPathResource("orders.json").getFile()))) {
 
-//             for (String line; (line = br.readLine()) != null;) {
-//                 order = objectMapper.readValue(line, TransportationOrder.class);
-//                 orders.add(order);
+            for (String line; (line = br.readLine()) != null;) {
+                order = objectMapper.readValue(line, TransportationOrder.class);
+                orders.add(order);
 
-//             }
-//         } catch (IOException e) {
-//             e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
 
-//         }
-//         return orders;
-//     }
-// }
+        }
+        return orders;
+    }
+}
